662736b51a906b744fd5631be55e1be8fbb1872c
Ferry <alessandro.ferrari.2004@gmail.com>
2020-06-17 21:19:14 -0400

    added some scripts

--- batPrice.py
+++ batPrice.py
@@ -2,14 +2,17 @@
 import requests
 
 url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=BAT"
-response = requests.get(
-        url,
-        headers={'Accept': 'application/json', 'X-CMC_PRO_API_KEY': 'a3f35322-d0de-4b38-adb2-3ad3aeef927c'},
-)
+try:
+    response = requests.get(
+            url,
+            headers={'Accept': 'application/json', 'X-CMC_PRO_API_KEY': 'a3f35322-d0de-4b38-adb2-3ad3aeef927c'},
+    )
 
-json = response.text
+    json = response.text
 
-price = json.split("price\":", 1)[1]
-price = round(float(price.split(",", 1)[0]), 4)
+    price = json.split("price\":", 1)[1]
+    price = round(float(price.split(",", 1)[0]), 4)
 
-print("⟁" + str(price))
+    print("⟁" + str(price))
+except:
+    print("⟁ Unavailable")
---
+++ plantData.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3.8
+
+# this will retrieve data about my plant
+
+import requests
+import re
+import json
+
+humidity = json.loads(requests.get("http://192.168.3.16:8080/rest/items/plantHumidityPercentage"))
+
+
+print(humidity.text)
