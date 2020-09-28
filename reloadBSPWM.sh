ff943bb56986ce3fe4d45e81715db005c1ae6079
Ferry <alessandroferry@protonmail.com>
2020-09-28 16:06:26 +0200

    back to ny

--- reloadBSPWM.sh
+++ reloadBSPWM.sh
@@ -2,4 +2,4 @@
 
 # this is so that I can call it in rofi
 
-/home/ferry/.config/bspwm/bspwmrc; notify-send 'bspwm' 'Reloaded config'
+/home/ferry/.config/bspwm/bspwmrc; notify-send --icon=$HOME/.local/share/icons/Custom/reload.png 'bspwm' 'Reloaded config'
--- reloadSxhkd.sh
+++ reloadSxhkd.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/sh
 
-pkill -usr1 -x sxhkd; notify-send 'sxhkd' 'Reloaded config'
+pkill -usr1 -x sxhkd; notify-send --icon=$HOME/.local/share/icons/Custom/reload.png 'sxhkd' 'Reloaded config'
