50056bb34eb05726800f0dccf56f79bce8f9f99d
Ferry <alessandroferry@protonmail.com>
2020-10-03 18:02:39 -0400

    added routine sctipt

---
+++ routine.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/sh
+
+upgrade() {
+echo -e "\n\033[0;37mStarting full system upgrade\033[0m\n"
+
+$HOME/Documents/scripts/fullUpgrade.sh
+}
+
+usage() {
+echo "This is a script to automate routine tasks."
+echo "By default, it just pushes dotfiles to master"
+echo "Flags:"
+echo "    -u | --upgrade -> also do a full system upgrade"
+echo "    -h | --help -> show this message"
+}
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -u | --upgrade )        upgrade
+                                ;;
+        -h | --help )           usage
+                                exit
+                                ;;
+        * )                     usage
+                                exit 1
+    esac
+    shift
+done
+
+
+echo -e "\n\033[0;32mPushing dotfile changes\033[0m\n" && yadm stage -u && yadm commit -m "routine commit" && yadm push origin master
