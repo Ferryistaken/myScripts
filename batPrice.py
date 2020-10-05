6995e201b4d3e3ec74a91e5a0e027d01e384c3e0
Ferry <alessandro.ferrari.2004@gmail.com>
2020-10-05 17:05:44 -0400

    routine push from argo

--- batPrice.py
+++ batPrice.py
@@ -8,20 +8,20 @@ if len(sys.argv) < 2:
     sys.exit(2)
 
 url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=BAT"
-#try:
-response = requests.get(
-        url,
-        headers={'Accept': 'application/json', 'X-CMC_PRO_API_KEY': 'a3f35322-d0de-4b38-adb2-3ad3aeef927c'},
-)
+try:
+    response = requests.get(
+            url,
+            headers={'Accept': 'application/json', 'X-CMC_PRO_API_KEY': 'a3f35322-d0de-4b38-adb2-3ad3aeef927c'},
+    )
 
-json = response.text
+    json = response.text
 
-price = json.split("price\":", 1)[1]
-price = round(float(price.split(",", 1)[0]), 4)
+    price = json.split("price\":", 1)[1]
+    price = round(float(price.split(",", 1)[0]), 4)
 
-coins = sys.argv[1]
-totalValue = round(price * float(coins), 2)
+    coins = sys.argv[1]
+    totalValue = round(price * float(coins), 2)
 
-print("⟁" + str(price) + " (" + str(totalValue) + ")")
-#except:
-print("⟁ Unavailable")
+    print("⟁" + str(price) + " (" + str(totalValue) + ")")
+except:
+    print("⟁ Unavailable")
