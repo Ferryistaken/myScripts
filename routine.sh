c0e8164729ab9f3f62e86bcdd7bb0d7497e27f79
Ferry <alessandro.ferrari.2004@gmail.com>
2020-10-29 22:00:22 -0400

    routine push from argo

--- routine.sh
+++ routine.sh
@@ -31,6 +31,9 @@ while [ "$1" != "" ]; do
     shift
 done
 
+echo -e "\n\033[0;32mCopying files to extra directory\033[0m\n"
+cp -r ~/.config/bspwm/ ~/.config/extras/argo/bspwm
+cp -r ~/.config/polybar/ ~/.config/extras/argo/polybar
 
 echo -e "\n\033[0;32mSyncing dotfiles\033[0m\n" && yadm pull origin master && yadm stage -u && yadm commit -m "routine commit from `hostname`" && yadm push origin master
 echo -e "\n\033[0;35mSyncing scripts\033[0m\n" && (cd $HOME/Documents/scripts;git pull origin master; git stage .; git commit -m "routine push from `hostname`"; git push origin master; cd -)
