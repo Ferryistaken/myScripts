f053d7cb6359442097ce7219aef37d3323862dd3
Ferry <alessandro.ferrari.2004@gmail.com>
2020-11-04 00:22:52 -0500

    routine push from argo

--- getMediaPlaying.sh
+++ getMediaPlaying.sh
@@ -6,7 +6,8 @@ mediaStatus=`playerctl metadata 2>&1`
 if [[ "$mediaStatus" == "No player could handle this command" ]]; then
 	echo "♫ No Player Found"
 else
-	title=`playerctl --player=playerctld metadata --format " ♫ {{ title }}" | cut -c1-30`
+	#title=`playerctl --player=playerctld metadata --format " ♫ {{ title }}" | cut -c1-30`
+	title=`playerctl --player=playerctld metadata --format " ♫ {{ title }}"`
 	#echo $title...
-	echo "$title... `playerctl --player=playerctld metadata --format " - {{ artist }}({{ status }})"`"
+	echo "$title`playerctl --player=playerctld metadata --format " - {{ artist }}({{ status }})"`"
 fi
